ce9dcd31dbdb963686b574eaf66f142e8ffabde8
valentinagonzalez21 <vgonzalez@correo.um.edu.uy>
2025-06-21 23:30:20 -0300

    fix credenciales para subir fotos

--- utils/uploadImageToS3.js
+++ utils/uploadImageToS3.js
@@ -1,8 +1,9 @@
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
+const s3 = require('../config/s3');
 
-const s3 = new S3Client({ region: process.env.AWS_REGION });
+//const s3 = new S3Client({ region: process.env.AWS_REGION });
 
 const uploadImageToS3 = async (file) => {
   const ext = path.extname(file.originalname); // .jpg, .png
